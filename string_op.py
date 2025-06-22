ffeeeeb4bebc28a5116c5c7759c22e943e26cde8
Aswin337 <aswinkumar11a106.2122@gmail.com>
2025-06-22 22:01:52 +0530

    string_op.py

---
+++ string_op.py
@@ -0,0 +1,10 @@
+a="i am aswin"
+fi=a.split()
+print(a.title())
+print(fi[2].capitalize())
+print(fi[2].upper())
+print(fi[2].lower())
+b="My rollno is : 23ucs114 "
+si=b.split(":")
+print(si[1])
+print(si[1].strip())
