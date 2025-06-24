1a484543f8bc0db22ec594a65fce7d61c2aa9514
Aswin337 <aswinkumar11a106.2122@gmail.com>
2025-06-24 14:46:56 +0530

    list_song_comp.py file added

--- list_song_comp.py
+++ list_song_comp.py
@@ -1,8 +1,8 @@
-songs=["ra ra rakamma","Naa ready","oo antava","jingucha"]
 def art_find(i):
     song_artist=["ra ra rakamma-aswin","naa ready-lognath","oo antava-anachi","jingucha-praveen"]
     for song in song_artist:
         if i.lower() in song:
-            return song
+            artist=song.replace(i.lower(),"").replace("-","").strip()
+            return artist
 res=art_find("OO ANTAVA")
 print(res)
